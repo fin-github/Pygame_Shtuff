f2e9613716b91a4db49b6d3b6f0fb04bfce96a42
Ace Rhino <a12n@mac.com>
2013-03-13 14:55:42 -0700

    Added board and square classes and expanded on them

--- Quarto_Interface.py
+++ Quarto_Interface.py
@@ -2,7 +2,7 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-MAINSURF = pygame.display.set_mode((800, 600))
+MAIN_SURF = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Quarto')
 
 WHITE = (255, 255, 255)
@@ -25,6 +25,8 @@ class Board():
         for square_num in range(self.total_squares):
             new_square = Square(square_num, self.position, self.square_width)
             self.squares.append(new_square)
+        width = (board_width * square_width) + (5 * (board_width - 1))
+        self.surface = pygame.Surface((width, width))
 
     def set_square(self, square_number, piece):
         pass
@@ -32,8 +34,16 @@ class Board():
     def get_square(self, square_number):
         pass
 
-    def get_board_surface():
-        pass
+    def get_board_surface(self):
+        for square in self.squares:
+            square_surf = square.get_square_surface()
+            self.surface.blit(square_surf, square.position)
+        self.surface.fill(BLACK)
+        return self.surface
+
+    def draw_board(self):
+        board_surf = self.get_board_surface()
+        MAIN_SURF.blit(board_surf, self.position)
     
 class Square():
 
@@ -41,7 +51,7 @@ class Square():
         self.square_num = square_number
         self.width = width
         #Do some math here!!
-        #self.position = 
+        self.position = (0,0)
 
     def get_val(self):
         pass
@@ -52,11 +62,27 @@ class Square():
     def get_location(self):
         return (self.x, self.y)
 
-    def get_square_surface():
+    def get_square_surface(self):
         pass
 
+def right_arrow_pressed():
+    print "Right Pressed!"
+
+def left_arrow_pressed():
+    print "Left Pressed!"
+
+def down_arrow_pressed():
+    print "Down Pressed!"
+
+def up_arrow_pressed():
+    print "Up Pressed!"
+
+MAIN_SURF.fill(WHITE)
+my_board = Board((100,50), 4, 100)
+my_board.draw_board()
+
 while True: # main game loop
-    MAINSURF.fill(WHITE)
+    #MAIN_SURF.fill(WHITE)
     for event in pygame.event.get():
         if(event.type == QUIT):
             pygame.quit()
