b2d7ac73af739a065bf650d13150deafcbde2b20
Ace Rhino <a12n@mac.com>
2013-03-14 13:58:43 -0700

    Further Board Updates (need to add images)

--- Quarto_Interface.py
+++ Quarto_Interface.py
@@ -20,13 +20,15 @@ class Board():
     def __init__(self, position, board_width, square_width):
         self.position = position
         self.total_squares = board_width * board_width
+        self.board_width = board_width
         self.square_width = square_width
         self.squares = []
         for square_num in range(self.total_squares):
-            new_square = Square(square_num, self.position, self.square_width, board_width)
+            new_square = Square(square_num, self)
             self.squares.append(new_square)
         width = (board_width * square_width) + (5 * (board_width - 1))
         self.surface = pygame.Surface((width, width))
+        self.board_rect = pygame.Rect(position[0], position[1], width, width)
 
     def set_square(self, square_number, piece):
         pass
@@ -44,19 +46,30 @@ class Board():
     def draw_board(self):
         board_surf = self.get_board_surface()
         MAIN_SURF.blit(board_surf, self.position)
+
+    def check_for_click(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        if self.board_rect.collidepoint([mouse_x, mouse_y]):
+            mouse_x -= self.position[0]
+            mouse_y -= self.position[1]
+            col = mouse_x / (self.square_width + 5)
+            row = mouse_y / (self.square_width + 5)
+            self.squares[(self.board_width * row) + col].on_click()
     
 class Square():
 
-    def __init__(self, square_number, board_position, square_width, squares_num):
+    print "TEST"
+
+    def __init__(self, square_number, board):
         self.square_num = square_number
-        self.width = square_width
-        row = square_number / squares_num
-        col = square_number % squares_num
-        x = (self.width + 5) * col
-        y = (self.width + 5) * row
-        self.position = (x,y)
+        self.board = board
+        self.width = self.board.square_width
+        row = self.square_num / self.board.board_width
+        col = self.square_num % self.board.board_width
+        self.position = ((self.width + 5) * col, (self.width + 5) * row)
         self.surface = pygame.Surface((self.width, self.width))
         self.surface.fill(GREEN)
+        self.holder = True
 
     def get_val(self):
         pass
@@ -70,6 +83,13 @@ class Square():
     def get_square_surface(self):
         return self.surface
 
+    def on_click(self):
+        if self.holder:
+            self.surface.fill(RED)
+        else:
+            self.surface.fill(GREEN)
+        self.holder = not self.holder
+    
 def right_arrow_pressed():
     print "Right Pressed!"
 
@@ -82,12 +102,11 @@ def down_arrow_pressed():
 def up_arrow_pressed():
     print "Up Pressed!"
 
-MAIN_SURF.fill(WHITE)
-my_board = Board((100,50), 4, 100)
-my_board.draw_board()
+my_board = Board((50,50), 4, 100)
 
 while True: # main game loop
-    #MAIN_SURF.fill(WHITE)
+    MAIN_SURF.fill(WHITE)
+    my_board.draw_board()
     for event in pygame.event.get():
         if(event.type == QUIT):
             pygame.quit()
@@ -102,10 +121,6 @@ while True: # main game loop
                 down_arrow_pressed()
             elif(key_map[K_UP]):
                 up_arrow_pressed()
-        elif(event.type == MOUSEMOTION):
-            #check_mouse_pos()
-            pass
         elif(event.type == MOUSEBUTTONDOWN):
-            #mouse_click()
-            pass
+            my_board.check_for_click()
     pygame.display.update()
