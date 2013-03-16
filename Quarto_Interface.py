24bbac0f2da6850e976f1bec94b0f90e08aa1aa2
Ace Rhino <a12n@mac.com>
2013-03-15 17:41:31 -0700

    Next_Piece_Square added

--- Quarto_Interface.py
+++ Quarto_Interface.py
@@ -93,7 +93,7 @@ class Square():
     def click_action(self, piece_num):
         if(self.piece == Square.EMPTY):
             raw_piece = pygame.image.load("Quarto_Pieces/Piece_"+str(piece_num)+".png")
-            self.piece = pygame.transform.scale(raw_piece, (100, 100))
+            self.piece = pygame.transform.scale(raw_piece, (self.width, self.width))
             self.surface.blit(self.piece, (0, 0))
             return False
         else:
@@ -104,23 +104,23 @@ class Piece_Holder():
     NO_CLICK = 7
     DONE_MOVING = -2
     RETURN_STEPS = 30
+    PIECE_WIDTH = 69
 
     def __init__(self, position, board):
         self.board = board
         self.position = position
-        self.surface = pygame.Surface((110, (55 * (board.total_squares / 2)) + 5))
-        pieces = []
+        self.surface = pygame.Surface(((Piece_Holder.PIECE_WIDTH + 5) * 2, ((Piece_Holder.PIECE_WIDTH + 5) * (board.total_squares / 2)) + 5))
+        self.squares = []
         for square in range(board.total_squares):
-            pieces.append(Holder_Square(square, self))
-        self.squares = pieces
-        self.holder_rect = pygame.Rect(position[0], position[1], 110, (55 * (board.total_squares / 2)) + 5)
+            self.squares.append(Holder_Square(square, self))
+        self.holder_rect = pygame.Rect(position[0], position[1], (Piece_Holder.PIECE_WIDTH + 5) * 2, ((Piece_Holder.PIECE_WIDTH + 5) * (board.total_squares / 2)) + 5)
         self.moving_piece = Piece_Holder.DONE_MOVING
         self.moving_piece_loc = (0, 0)
 
 
     def update_holder_surface(self):
         self.surface.fill(WHITE)
-        pygame.draw.rect(self.surface, BLACK, (0, 0, 110, (55 * (self.board.total_squares / 2)) + 5), 5)
+        #pygame.draw.rect(self.surface, BLACK, (0, 0, (Piece_Holder.PIECE_WIDTH + 5) * 2, ((Piece_Holder.PIECE_WIDTH + 5) * (self.board.total_squares / 2)) + 5), 5)
         for square in self.squares:
             square_surf = square.get_square_surface()
             self.surface.blit(square_surf, square.position)
@@ -137,8 +137,8 @@ class Piece_Holder():
         if self.holder_rect.collidepoint([mouse_x, mouse_y]):
             mouse_x -= self.position[0]
             mouse_y -= self.position[1]
-            col = mouse_x / (55)
-            row = mouse_y / (55)
+            col = mouse_x / ((Piece_Holder.PIECE_WIDTH + 5))
+            row = mouse_y / ((Piece_Holder.PIECE_WIDTH + 5))
             clicked_piece = self.squares[(row * 2) + col].click_action()
             if(clicked_piece == Holder_Square.EMPTY):
                 return (Piece_Holder.NO_CLICK, 0)
@@ -176,11 +176,16 @@ class Holder_Square():
     def __init__(self, square_number, piece_holder):
         self.square_num = square_number
         self.holder = piece_holder
-        self.position = ((self.square_num % 2) * 55, ((self.square_num / 2) * 55) + 5)
-        self.surface = pygame.Surface((52, 52))
-        raw_piece = pygame.image.load("Quarto_Pieces/Piece_"+str(self.square_num)+".png")
-        self.primary_piece = pygame.transform.scale(raw_piece, (50, 50))
-        self.piece = self.primary_piece
+        self.position = ((self.square_num % 2) * (Piece_Holder.PIECE_WIDTH + 5),
+                         ((self.square_num / 2) * (Piece_Holder.PIECE_WIDTH + 5)) + 5)
+        self.surface = pygame.Surface(((Piece_Holder.PIECE_WIDTH + 2), (Piece_Holder.PIECE_WIDTH + 2)))
+        if(self.square_num != 15):
+            raw_piece = pygame.image.load("Quarto_Pieces/Piece_"+str(self.square_num)+".png")
+            self.primary_piece = pygame.transform.scale(raw_piece, (Piece_Holder.PIECE_WIDTH, Piece_Holder.PIECE_WIDTH))
+            self.piece = self.primary_piece
+        else:
+            self.primary_piece = Holder_Square.EMPTY
+            self.piece = self.primary_piece
         
     def get_piece(self):
         return self.piece
@@ -190,7 +195,8 @@ class Holder_Square():
 
     def get_square_surface(self):
         self.surface.fill(WHITE)
-        pygame.draw.rect(self.surface, BLACK, (0, 0, 52, 52), 5)
+        pygame.draw.rect(self.surface, BLACK, (0, 0, (Piece_Holder.PIECE_WIDTH + 2),
+                                               (Piece_Holder.PIECE_WIDTH + 2)), 5)
         if (self.piece != Holder_Square.EMPTY):
             self.surface.blit(self.piece, (1, 1))
         return self.surface
@@ -203,22 +209,78 @@ class Holder_Square():
     def returned(self):
         self.piece = self.primary_piece
 
+class Next_Piece_Box():
 
-def move_piece_back():
-    pass
+    def __init__(self, position):
+        self.position = position
+        self.surface = pygame.Surface((Piece_Holder.PIECE_WIDTH + 2, Piece_Holder.PIECE_WIDTH + 2))
+        raw_piece = pygame.image.load("Quarto_Pieces/Piece_15.png")
+        self.piece = pygame.transform.scale(raw_piece, (Piece_Holder.PIECE_WIDTH, Piece_Holder.PIECE_WIDTH))
+        self.square_rect = pygame.Rect(position[0], position[1], Piece_Holder.PIECE_WIDTH + 2, Piece_Holder.PIECE_WIDTH + 2)
+        self.piece_num = 15
 
-my_board = Board((300, 50), 4, 100)
-my_piece_holder = Piece_Holder((10, 10), my_board)
+    def get_current_piece(self):
+        return self.piece
+
+    def update_box_surface(self):
+        self.surface.fill(WHITE)
+        pygame.draw.rect(self.surface, BLACK, (0, 0, (Piece_Holder.PIECE_WIDTH + 2),
+                                               (Piece_Holder.PIECE_WIDTH + 2)), 5)
+        if(self.piece != Holder_Square.EMPTY):
+            self.surface.blit(self.piece, (1, 1))
+        return self.surface
+
+    def draw_box(self):
+        self.surface = self.update_box_surface()
+        MAIN_SURF.blit(self.surface, self.position)
+
+    def check_for_click(self, piece_num):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        if self.square_rect.collidepoint([mouse_x, mouse_y]):
+            if(self.piece != Holder_Square.EMPTY):
+                place_holder = (self.piece, self.piece_num)
+                self.piece = Holder_Square.EMPTY
+                self.piece_num = Holder_Square.EMPTY
+                return place_holder
+            else:
+                self.on_mouse_drop(piece_num)
+        else:
+            return (Piece_Holder.NO_CLICK, 0)
+
+    def on_mouse_drop(self, piece_num):
+        self.piece_num = piece_num
+        raw_piece = pygame.image.load("Quarto_Pieces/Piece_"+str(piece_num)+".png")
+        self.piece = pygame.transform.scale(raw_piece, (Piece_Holder.PIECE_WIDTH, Piece_Holder.PIECE_WIDTH))
+        self.surface.blit(self.piece, (1, 1))
+            
+
+my_board = Board((350, 88), 4, 100)
+my_piece_holder = Piece_Holder((10, 0), my_board)
+my_next_piece_box = Next_Piece_Box((200, 250))
 dragging_piece = False
 piece_moving = False
-#PIECE_1 = pygame.image.load("Quarto_Pieces/Piece_1.jpg")
-#PIECE_1 = pygame.transform.scale(PIECE_1, (100, 100))
+piece_num = -1
 
+def mouse_button_down():
+    global piece_moving, dragging_piece, piece_clicked, piece_num
+    
+    if(not piece_moving):
+        if(my_next_piece_box.get_current_piece() == Holder_Square.EMPTY):
+            piece_clicked, piece_num = my_piece_holder.check_for_click()
+            if(piece_clicked != Piece_Holder.NO_CLICK):
+                dragging_piece = True
+        else:
+            piece_clicked, piece_num = my_next_piece_box.check_for_click()
+            print piece_clicked
+            print piece_num
+            if(piece_clicked != Piece_Holder.NO_CLICK):
+                dragging_piece = True
 
 while True: # main game loop
     MAIN_SURF.fill(WHITE)
     my_board.draw_board()
     my_piece_holder.draw_holder()
+    my_next_piece_box.draw_box()
     
     for event in pygame.event.get():
         if(event.type == QUIT):
@@ -227,10 +289,7 @@ while True: # main game loop
         elif(event.type == KEYDOWN):
             key_map = pygame.key.get_pressed()
         elif(event.type == MOUSEBUTTONDOWN):
-            if(not piece_moving):
-                piece_clicked, piece_num = my_piece_holder.check_for_click()
-                if (piece_clicked != Piece_Holder.NO_CLICK):
-                    dragging_piece = True
+            mouse_button_down()
         elif(event.type == MOUSEBUTTONUP):
             if(dragging_piece):
                 dragging_piece = False
