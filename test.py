6b9abf994daae7ea114fcaaf33641bb6bfc3ddea
Ace Rhino <a12n@mac.com>
2013-03-18 20:48:31 -0700

    One More OpenGL Example

---
+++ test.py
@@ -0,0 +1,15 @@
+import pygame, sys
+from pygame.locals import *
+import pygame.examples.glcube
+
+pygame.init()
+MAIN_SURF = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Quarto')
+
+while True: # main game loop
+    pygame.examples.glcube.main()
+    
+    for event in pygame.event.get():
+        if(event.type == QUIT):
+            pygame.quit()
+            sys.exit()
